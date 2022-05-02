aa0e05801319f8059c00c43cfb706074d8cbe7b2
yulixr <66062982+yulixr@users.noreply.github.com>
2022-05-02 19:23:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,28 +7,27 @@ def F(x):
 def Quads(func, n, metod, a, b):
   summ = 0
   y = []
+  x = []
   h = (b - a) / n
   start = a + metod * h
   for i in range(n):
     summ += func(start + i * h)
     y.append(func(start+i * h))
-  return summ * h, y
+    x.append(start + i * h)
+  return summ * h, y, x
 
 def Integral(func, n, metod, a, b):
   if metod == 0:
-    res, y = Quads(func, n, 0.0, a, b)
-    x = np.linspace(a, b, n)
-    plt.bar(x, y, width=1/n,edgecolor="k", align = "edge")
+    res, y, x = Quads(func, n, 0.0, a, b)
+    plt.bar(x, y, width=1/n * 2,edgecolor="k", align = "edge")
     return res
   elif metod == 1:
-    res, y = Quads(func, n, 1.0, a, b)
-    x = np.linspace(a, b, n)
-    plt.bar(x, y, width=-1/n,edgecolor="k", align = "edge")
+    res, y, x = Quads(func, n, 1.0, a, b)
+    plt.bar(x, y, width=-1/n *2,edgecolor="k", align = "edge")
     return res
   elif metod == 2:
-    res, y = Quads(func, n, 0.5, a, b)
-    x = np.linspace(a, b, n)
-    plt.bar(x, y, width=1/n,edgecolor="k")
+    res, y, x = Quads(func, n, 0.5, a, b)
+    plt.bar(x, y, width=1/n * 2,edgecolor="k")
     return res
 
 print("Input a b:")
@@ -42,5 +41,6 @@ y_ideal = [F(a) for a in x]
 plt.plot(x, y_ideal, color = "orange")
 res = Integral(F, n, metod, a, b)
 plt.grid()
-plt.title("Integral " + str(round(res, 2)) + " with metod " + ("left rectangles" if(metod == 0) else("right rectangles" if (metod == 1) else "middle rectangles")))
+plt.title("Integral " + str(round(res, 2)) + " with metod " + ("left rectangles" if(metod == 0) else("right rectangles" if (metod == 1) else "middle rectangles")) + " n = " + str(n))
+plt.savefig("f" + str(round(res, 2)) + " with metod " + str(metod) + str(n) +".png" )
 plt.show()
